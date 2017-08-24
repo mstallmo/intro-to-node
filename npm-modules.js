3c99aff6ccf310d14d009bdd94b7f97e1b8f5940
Mason Stallmo <masonstallmo@gmail.com>
2017-08-23 20:07:24 -0500

    Add npm modules to intro demo

---
+++ npm-modules.js
@@ -0,0 +1,7 @@
+const request = require('request');
+
+request('http://www.pluralsight.com/', function (err, response, body) {
+    if (!err && response.statusCode === 200) {
+        console.log(body);
+    }
+});
