2815bfbf647be1def242890a37e6d443be107010
Mason Stallmo <masonstallmo@gmail.com>
2017-08-23 19:06:00 -0500

    First example writing callbacks

---
+++ callback.js
@@ -0,0 +1,35 @@
+const maxTime = 1000;
+
+let evenDoubler = function(v, callback){
+    let waitTime = Math.floor(Math.random()*(maxTime+1));
+    if (v%2){
+        setTimeout(function() {
+            callback(new Error("Odd Input"));
+        }, waitTime);
+    }
+    else {
+        setTimeout(function() {
+            callback(null, v*2, waitTime);
+        }, waitTime);
+    }
+};
+
+let count = 0;
+
+for (let i = 0; i < 10; ++i) {
+    console.log("Calling evenDoubler for value " + i);
+    evenDoubler(i, function(err, results, time) {
+        if (err) {
+            console.log("Error: " + err.message);
+        }
+        else {
+            console.log("The results are: " + results + " ("+ time + " ms");
+        }
+
+        if (++count === 10) {
+            console.log("Done!");
+        }
+    });
+}
+
+console.log("------");
